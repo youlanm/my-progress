db85b607d42ebab4ecdc7957018809d9f9155b0d
youlanm <youlanm@users.noreply.github.com>
2026-08-19 21:25:49 +0800

    update project1.slnx

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,30 +1,10 @@
 ﻿#include<iostream>
-using namespace std;   //std::
+using namespace std;  
 
 int main()
 {
 
-	cout << "1"<<endl;
-
-	system("pause");
-
-	cout << "2" <<endl;
-
-	system("pause");
-
-	cout << "3"<<endl;
-
-	system("pause");
-
-	cout << "4"<<endl;
-
-	system("pause");
-
-	cout << "5" << endl;
-
-	system("pause");
-
-	cout << "6" <<endl ;
+	cout << "Hello,world!"<<endl;
 
 	system("pause");
 
