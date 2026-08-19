f767de16d7bdefc9161b470cf15d47f2ebebaa91
youlanm <youlanm@users.noreply.github.com>
2026-08-19 18:26:38 +0800

    Initial commit

---
+++ Project1/main.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+using namespace std;   //std::
+
+int main()
+{
+
+	cout << "1"<<endl;
+
+	system("pause");
+
+	cout << "2" <<endl;
+
+	system("pause");
+
+	cout << "3"<<endl;
+
+	system("pause");
+
+	cout << "4"<<endl;
+
+	system("pause");
+
+	cout << "5" << endl;
+
+	system("pause");
+
+	cout << "6" <<endl ;
+
+	system("pause");
+
+	return 0;
+}
