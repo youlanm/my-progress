634820848f9fa5ef7b415893b71b660d8e063d3b
youlanm <youlanm@users.noreply.github.com>
2026-09-19 14:44:30 +0800

    computing

---
+++ computing/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 1, b = 12, c = 3, d = 4;
+
+	cout << a + b << a + c<< endl;
+
+	system("pause");
+
+	cout << c * d << endl;
+
+	system("pause");
+
+	return 0;
+}
